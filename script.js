f6152aa621771dccab1dd2d58698529e29cdc806
Ariel <Ariel.abarca7@gmail.com>
2021-08-27 17:49:01 -0300

    Commit final

---
+++ script.js
@@ -0,0 +1,29 @@
+// Prompt que solicita un número entre 1 y 20
+let num = +prompt("Por favor, ingrese un número entre 1 y 20");
+
+// Función que verifica el rango del número ingresado
+const check = (number) => {
+    if (number >= 1 && number <= 20) {
+
+        // Ciclo for para multiplicar el numero ingresado
+        for (let multiplier = 1; multiplier <= number; multiplier++) {
+            console.log(`${multiplier} x ${number} = ${multiplier * number}`);
+        }
+
+        // Ciclos for que obtienen la factorial
+        for (let factorial = 1; factorial <= number; factorial++) {
+
+            let factorialResult = 1;
+
+            for (let factorialIteration = 1; factorialIteration <= factorial; factorialIteration++) {
+                factorialResult = factorialResult * factorialIteration;
+
+            }
+            console.log(`Factorial de ${factorial} es: ${factorialResult}`);
+        }
+
+    } else console.log("Número fuera de rango");
+}
+
+// Se imprime el resultado en consola
+console.log(check(num));
